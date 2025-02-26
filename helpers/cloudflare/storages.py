441fb0757c17b1305c90610b8c749861b8281af7
James Woodhall <woodhalljames@gmail.com>
2025-02-26 13:30:57 -0500

    Update storages.py

--- helpers/cloudflare/storages.py
+++ helpers/cloudflare/storages.py
@@ -3,5 +3,17 @@ from storages.backends.s3 import S3Storage
 class StaticFileStorage(S3Storage):
     location = "static"
     
+    def _normalize_name(self, name):
+        # Prevent bucket name duplication
+        if name.startswith(self.bucket_name + '/'):
+            name = name[len(self.bucket_name) + 1:]
+        return super()._normalize_name(name)
+
 class MediaFileStorage(S3Storage):
     location = "media"
+    
+    def _normalize_name(self, name):
+        # Prevent bucket name duplication
+        if name.startswith(self.bucket_name + '/'):
+            name = name[len(self.bucket_name) + 1:]
+        return super()._normalize_name(name)
