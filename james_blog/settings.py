48a8e6f0a3133654f2629bdf68d234e0c3a293fa
James <woodhalljames@gmail.com>
2025-02-26 15:54:32 -0500

    prod push from term

--- james_blog/settings.py
+++ james_blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
-ALLOWED_HOSTS = ['jameswoodhall.com','www.jameswoodhall.com', '*']
+ALLOWED_HOSTS = ['jameswoodhall.com','www.jameswoodhall.com', '*', '.vercel.app']
 
 
 SECURE_SSL_REDIRECT = True
