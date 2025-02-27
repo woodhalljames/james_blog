9798a37fd77ddb115857a26540950e941038953e
James <woodhalljames@gmail.com>
2025-02-27 12:07:51 -0500

    debug and CV

--- james_blog/settings.py
+++ james_blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = config("DEBUG", cast = bool, default = False)
 ALLOWED_HOSTS = ['jameswoodhall.com','www.jameswoodhall.com', '*', '.vercel.app']
 
 
@@ -136,7 +136,8 @@ STORAGES = {
         "OPTIONS": CLOUDFLARE_R2_CONFIG_OPTIONS
     }, #default user/image/file uploads
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
+        "BACKEND": "helpers.cloudflare.storages.StaticFileStorage",
+        "OPTIONS": CLOUDFLARE_R2_CONFIG_OPTIONS
     } #staticfiles
 }
 
